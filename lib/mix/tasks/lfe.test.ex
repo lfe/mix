bd2f56b1908447b85552690154aada37b9dffa4b
John Goff <john.g@hiredhippo.ai>
2024-02-07 21:56:10 -0500

    Run ltest in test environment

--- lib/mix/tasks/lfe.test.ex
+++ lib/mix/tasks/lfe.test.ex
@@ -5,6 +5,7 @@ defmodule Mix.Tasks.Lfe.Test do
   @recursive true
   @manifest "test.lfe"
   @switches [force: :boolean, all_warnings: :boolean]
+  @preferred_cli_env :test
 
   @shortdoc "Runs a LFE project's tests"
 
@@ -22,37 +23,23 @@ defmodule Mix.Tasks.Lfe.Test do
   Runs this task.
   """
   def run(args) do
+    Logger.configure(level: :info)
     {opts, _, _} = OptionParser.parse(args, switches: @switches)
-    Mix.env(:test)
 
-    do_run(opts)
+    dest = Mix.Project.compile_path()
+
+    File.rmdir(dest)
+    File.mkdir_p!(dest)
+
+    Lfe.compile(manifest(), [{"src", dest}, {"test", dest}], opts)
+
+    :ltest.all()
   end
 
   @doc """
   Returns LFE test manifests.
   """
   def manifests, do: [manifest()]
-
-  defp do_run(opts) do
-    dest = Mix.Project.compile_path() |> String.replace("_build/dev", "_build/test")
-    location = String.split(dest, "_build") |> List.last()
-    {:ok, root} = File.cwd()
-    dest = root |> Path.join("_build") |> Path.join(location)
-
-    File.rmdir(dest)
-    File.mkdir_p!(dest)
-
-    dest_test = dest |> Path.join("_build") |> Path.join(location)
-
-    File.rmdir(dest_test)
-    File.mkdir_p!(dest_test)
-
-    Lfe.compile(manifest(), [{"src", dest}, {"test", dest_test}], opts)
-
-    File.cd(dest)
-
-    :"ltest-runner".unit()
-  end
 
   defp manifest, do: Path.join(Mix.Project.manifest_path(), @manifest)
 end

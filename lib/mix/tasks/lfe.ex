700c68757f48652be162c89c8927bd00a2869e79
John Goff <john.g@hiredhippo.ai>
2024-02-11 17:40:50 -0500

    Start the LFE shell through the erlang shell module

--- lib/mix/tasks/lfe.ex
+++ lib/mix/tasks/lfe.ex
@@ -11,18 +11,12 @@ defmodule Mix.Tasks.Lfe do
 
   def run(_args) do
     IO.puts(:lfe_shell.banner())
-    # Elixir sets the IO opts to return input as binaries, which messes up the
-    # LFE shell which expects charlists.
-    gl = Process.group_leader()
-    before_shell_spawns_opts = :io.getopts(gl)
-    :io.setopts(gl, binary: false)
-    # We must monitor the shell process so the task doesn't end immediately.
-    pid = :lfe_shell.start()
-    ref = Process.monitor(pid)
+    # Think this is OTP 26+, need to figure out what to do on lower versions
+    :shell.start_interactive({:lfe_shell, :start, []})
+    ref = Process.monitor(:shell.whereis())
 
     receive do
       {:DOWN, ^ref, _, _} ->
-        :io.setopts(gl, before_shell_spawns_opts)
         :ok
     end
   end
